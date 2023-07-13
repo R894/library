218d10d767d5a1d491692d715dbadc96e80caf87
R894 <kamil.khalaileh@gmail.com>
2023-07-13 08:48:41 +0300

    Refactor script.js to use classes

--- script.js
+++ script.js
@@ -1,21 +1,88 @@
-let myLibrary = [];
+class Book {
+    constructor(title, author, pages, read){
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+        this.id = null;
+    }
 
-function Book(title, author, pages, read){
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.info = function(){
+    info(){
         if(!this.read){
             return "Not read yet";
         } else {
             return "Read";
         }
     }
-    this.toggleRead = function(){
+
+    toggleRead(){
         this.read = !this.read;
     }
+
+    setId(id){
+        this.id = id
+    }
+
+    getId(){
+        return this.id;
+    }
 }
+
+class Library {
+    constructor(){
+        this.myLibrary = [];
+    }
+
+    addBook(book){
+        this.myLibrary.push(book);
+        book.setId(this.myLibrary.indexOf(book));
+        this.displayBook(book);
+    }
+
+    removeBook(book){
+        document.querySelector(`[data="${book.getId()}"]`).remove();
+    }
+
+    displayBook(book){
+        var card = document.createElement('div');
+        var title = document.createElement('div');
+        var author = document.createElement('div');
+        var pages = document.createElement('div');
+        var bookRead = document.createElement('div');
+        var bookReadButton = document.createElement('button');
+        var removeButton = document.createElement('button');
+    
+        card.classList.add('book');
+        card.setAttribute('data', book.getId());
+        removeButton.textContent = 'Remove';
+        title.textContent = `Title: ${book.title}`;
+        author.textContent = `Author: ${book.author}`;
+        pages.textContent = `Pages: ${book.pages}`;
+    
+        bookRead.textContent = book.info();
+        bookReadButton.textContent = 'Toggle Read';
+    
+        removeButton.addEventListener("click", () => {
+            this.removeBook(book);
+        });
+    
+        bookReadButton.addEventListener("click", function(){
+            book.toggleRead();
+            bookRead.textContent = book.info();
+        })
+    
+        card.appendChild(title);
+        card.appendChild(author);
+        card.appendChild(pages);
+        card.appendChild(bookRead);
+        card.appendChild(bookReadButton);
+        card.appendChild(removeButton);
+    
+        cardContainer.appendChild(card);
+    }
+}
+
+var lib = new Library();
 
 var addBookButton = document.getElementById("add-book");
 var formContainer = document.getElementById("form-container");
@@ -30,57 +97,11 @@ form.addEventListener("submit", function(event){
     const read = document.querySelector("#read").checked;
     form.reset();
 
-    addBookToLibrary(new Book(title, author, pages, read));
-
+    lib.addBook(new Book(title, author, pages, read));
 });
 
 addBookButton.addEventListener("click", function(){
     formContainer.style.display = (formContainer.style.display === 'none') ? 'flex' : 'none';
 });
 
-function displayBook(book){
-    var card = document.createElement('div');
-    var title = document.createElement('div');
-    var author = document.createElement('div');
-    var pages = document.createElement('div');
-    var bookRead = document.createElement('div');
-    var bookReadButton = document.createElement('button');
-    var removeButton = document.createElement('button');
 
-    card.classList.add('book');
-    card.setAttribute('data', myLibrary.indexOf(book));
-    removeButton.textContent = 'Remove';
-    title.textContent = `Title: ${book.title}`;
-    author.textContent = `Author: ${book.author}`;
-    pages.textContent = `Pages: ${book.pages}`;
-
-    bookRead.textContent = book.info();
-    bookReadButton.textContent = 'Toggle Read';
-
-    removeButton.addEventListener("click", function(){
-        removeBook(myLibrary.indexOf(book));
-    });
-
-    bookReadButton.addEventListener("click", function(){
-        book.toggleRead();
-        bookRead.textContent = book.info();
-    })
-
-    card.appendChild(title);
-    card.appendChild(author);
-    card.appendChild(pages);
-    card.appendChild(bookRead);
-    card.appendChild(bookReadButton);
-    card.appendChild(removeButton);
-
-    cardContainer.appendChild(card);
-}
-
-function addBookToLibrary(book){
-    myLibrary.push(book);
-    displayBook(book);
-}
-
-function removeBook(id){
-    document.querySelector(`[data="${id}"]`).remove();
-}
